416999878a2fe4f96faa4d364f6d95c0320eef37
wiggin77 <ender.wiggin@warpmail.net>
2015-08-21 18:19:50 -0400

    Test app: Updated package.json for Mongoose dependency

--- test/src/com/dal/mongotest/Config.hx
+++ test/src/com/dal/mongotest/Config.hx
@@ -5,7 +5,9 @@ class Config
 	public static inline var 	username  	= "testUser";
 	public static inline var 	password  	= "test";
 
-	public static inline var 	domain 		= "192.168.1.83"; //"localhost";
+	//public static inline var 	domain 		= "192.168.1.83"; 
+	public static inline var 	domain 		= "localhost";
+
 	public static inline var 	port  		= "27017";
 	public static inline var 	database  	= "test";
 
